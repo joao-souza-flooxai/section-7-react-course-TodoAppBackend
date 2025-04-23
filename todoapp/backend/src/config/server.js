d304328a67cd218c71251fb3a468053378443b2b
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-23 13:07:24 -0300

    feat: Added the middleware to allow cors. Finished the backend for now.

--- todoapp/backend/src/config/server.js
+++ todoapp/backend/src/config/server.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
+const cors = require("./cors");
 const express = require('express');
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cors);
 
 const port = 3003; 
 server.listen(port, function() {
