52f8e0b5951f1947ce3fd8c29d7853fa949277c3
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-23 14:44:03 -0300

    fix: Fixed some depedency problems due diffence on the course and the files provided by it. feat: Created the first component.

---
+++ todoapp/frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+
+export default function App(props){
+
+    return  (
+        <div className='container'>
+            <h1>Teste</h1>
+        </div>
+    );
+}
